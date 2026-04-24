a9255d695e571c54e34776ef4ccfb59655675634
Luavis <luaviskang@gmail.com>
2026-04-24 12:59:43 +0900

    Fix nvim/zsh and add .macos to run

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.opt.exrc = true
-vim.opt.secure = true
-vim.opt.autoread = true
-vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
-  command = "checktime",
-})
-
-local wk = require("which-key")
-wk.add({
-  { "<leader>o", group = "opencode", icon = "" },
-})
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,14 +1,3 @@
-vim.g.omni_sql_no_default_maps = 1
-vim.g.lazyvim_python_lsp = "ty"
-vim.g.lazyvim_python_ruff = "ruff"
-
-vim.opt.whichwrap:append({
-  ["<"] = true,
-  [">"] = true,
-  ["["] = true,
-  ["]"] = true,
-  h = true,
-  l = true,
-})
-
-vim.g.lazyvim_blink_main = true
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
